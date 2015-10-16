4e6d34cc26746ae172bb5599a0f7c71d0d2b9c0c
Your Name <johny@gmail.com>
2015-10-16 08:51:08 +0200

    throw exception if user table already exists in inital migration. See issue #81

--- migrations/m140608_173539_create_user_table.php
+++ migrations/m140608_173539_create_user_table.php
@@ -16,23 +16,19 @@ class m140608_173539_create_user_table extends Migration
 	        // Check if user Table exist
 	        // $tablename = \Yii::$app->db->tablePrefix.'user';
 	        $tablename = \Yii::$app->getModule('user-management')->user_table;
-	        if (\Yii::$app->db->schema->getTableSchema($tablename) === null)
-	        {
-			// Create user table
-			$this->createTable($tablename, array(
-				'id'                 => 'pk',
-				'username'           => 'string not null',
-				'auth_key'           => 'varchar(32) not null',
-				'password_hash'      => 'string not null',
-				'confirmation_token' => 'string',
-				'status'             => 'int not null default 1',
-				'superadmin'         => 'smallint default 0',
-				'created_at'         => 'int not null',
-				'updated_at'         => 'int not null',
-			), $tableOptions);
-	        }
 
-
+		// Create user table
+		$this->createTable($tablename, array(
+			'id'                 => 'pk',
+			'username'           => 'string not null',
+			'auth_key'           => 'varchar(32) not null',
+			'password_hash'      => 'string not null',
+			'confirmation_token' => 'string',
+			'status'             => 'int not null default 1',
+			'superadmin'         => 'smallint default 0',
+			'created_at'         => 'int not null',
+			'updated_at'         => 'int not null',
+		), $tableOptions);
 	}
 
 	public function safeDown()
