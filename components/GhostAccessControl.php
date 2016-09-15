11586250e729a234c486a0976c8f794c338df9a6
Stunc0 <nicolas.mauffre@gmail.com>
2016-09-15 18:06:07 +0200

    Adding correction to be in phase with IE login redirection and parameters added since YII v2.0.8

--- components/GhostAccessControl.php
+++ components/GhostAccessControl.php
@@ -100,7 +100,7 @@ class GhostAccessControl extends ActionFilter
 	{
 		if ( Yii::$app->user->getIsGuest() )
 		{
-			Yii::$app->user->loginRequired();
+			Yii::$app->user->loginRequired(Yii::$app->request->isAjax, (strpos(Yii::$app->request->headers->get('Accept'), 'html') !== false));
 		}
 		else
 		{
@@ -108,4 +108,4 @@ class GhostAccessControl extends ActionFilter
 		}
 	}
 
-} 
+}
