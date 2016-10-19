d64fbe90ce5f93dad2272c4271ab484307c8faef
Stunc0 <nicolas.mauffre@gmail.com>
2016-10-19 18:47:49 +0200

    Coorection on french language back. Impacted by changes on front.

--- messages/fr/back.php
+++ messages/fr/back.php
@@ -52,7 +52,7 @@ return [
     'Inactive' => 'Inactif',
     'IP' => '',
     'Language' => 'Langage',
-    'Login' => 'Se connecter',
+    'Login' => 'Identifiant',
     'Name' => 'Nom',
     'OS' => 'OS',
     'Password' => 'Mot de passe',
