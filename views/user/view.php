0f8d4f8990d9de7b031fecece5b5d557d300a844
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2017-03-15 13:19:23 +0100

    style: Honor PSR-2 coding style guidelines

--- views/user/view.php
+++ views/user/view.php
@@ -8,7 +8,7 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
-/**
+/*
  * @var yii\web\View $this
  * @var webvimark\modules\UserManagement\models\User $model
  */
@@ -19,71 +19,71 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-view">
 
-	<h2 class="lte-hide-title"><?= $this->title ?></h2>
+    <h2 class="lte-hide-title"><?= $this->title ?></h2>
 
-	<div class="panel panel-default">
-		<div class="panel-body">
+    <div class="panel panel-default">
+        <div class="panel-body">
 
-		    <p>
-			<?= GhostHtml::a(UserManagementModule::t('back', 'Edit'), ['update', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary']) ?>
-			<?= GhostHtml::a(UserManagementModule::t('back', 'Create'), ['create'], ['class' => 'btn btn-sm btn-success']) ?>
-			<?= GhostHtml::a(
-				UserManagementModule::t('back', 'Roles and permissions'),
-				['/user-management/user-permission/set', 'id'=>$model->id],
-				['class' => 'btn btn-sm btn-default']
-			) ?>
+            <p>
+            <?= GhostHtml::a(UserManagementModule::t('back', 'Edit'), ['update', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary']) ?>
+            <?= GhostHtml::a(UserManagementModule::t('back', 'Create'), ['create'], ['class' => 'btn btn-sm btn-success']) ?>
+            <?= GhostHtml::a(
+                UserManagementModule::t('back', 'Roles and permissions'),
+                ['/user-management/user-permission/set', 'id' => $model->id],
+                ['class' => 'btn btn-sm btn-default']
+            ) ?>
 
-			<?= GhostHtml::a(UserManagementModule::t('back', 'Delete'), ['delete', 'id' => $model->id], [
-			    'class' => 'btn btn-sm btn-danger pull-right',
-			    'data' => [
-				'confirm' => UserManagementModule::t('back', 'Are you sure you want to delete this user?'),
-				'method' => 'post',
-			    ],
-			]) ?>
-		    </p>
+            <?= GhostHtml::a(UserManagementModule::t('back', 'Delete'), ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-sm btn-danger pull-right',
+                'data' => [
+                'confirm' => UserManagementModule::t('back', 'Are you sure you want to delete this user?'),
+                'method' => 'post',
+                ],
+            ]) ?>
+            </p>
 
-			<?= DetailView::widget([
-				'model'      => $model,
-				'attributes' => [
-					'id',
-					[
-						'attribute'=>'status',
-						'value'=>User::getStatusValue($model->status),
-					],
-					'username',
-					[
-						'attribute'=>'email',
-						'value'=>$model->email,
-						'format'=>'email',
-						'visible'=>User::hasPermission('viewUserEmail'),
-					],
-					[
-						'attribute'=>'email_confirmed',
-						'value'=>$model->email_confirmed,
-						'format'=>'boolean',
-						'visible'=>User::hasPermission('viewUserEmail'),
-					],
-					[
-						'label'=>UserManagementModule::t('back', 'Roles'),
-						'value'=>implode('<br>', ArrayHelper::map(Role::getUserRoles($model->id), 'name', 'description')),
-						'visible'=>User::hasPermission('viewUserRoles'),
-						'format'=>'raw',
-					],
-					[
-						'attribute'=>'bind_to_ip',
-						'visible'=>User::hasPermission('bindUserToIp'),
-					],
-					array(
-						'attribute'=>'registration_ip',
-						'value'=>Html::a($model->registration_ip, "http://ipinfo.io/" . $model->registration_ip, ["target"=>"_blank"]),
-						'format'=>'raw',
-						'visible'=>User::hasPermission('viewRegistrationIp'),
-					),
-					'created_at:datetime',
-					'updated_at:datetime',
-				],
-			]) ?>
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'id',
+                    [
+                        'attribute' => 'status',
+                        'value' => User::getStatusValue($model->status),
+                    ],
+                    'username',
+                    [
+                        'attribute' => 'email',
+                        'value' => $model->email,
+                        'format' => 'email',
+                        'visible' => User::hasPermission('viewUserEmail'),
+                    ],
+                    [
+                        'attribute' => 'email_confirmed',
+                        'value' => $model->email_confirmed,
+                        'format' => 'boolean',
+                        'visible' => User::hasPermission('viewUserEmail'),
+                    ],
+                    [
+                        'label' => UserManagementModule::t('back', 'Roles'),
+                        'value' => implode('<br>', ArrayHelper::map(Role::getUserRoles($model->id), 'name', 'description')),
+                        'visible' => User::hasPermission('viewUserRoles'),
+                        'format' => 'raw',
+                    ],
+                    [
+                        'attribute' => 'bind_to_ip',
+                        'visible' => User::hasPermission('bindUserToIp'),
+                    ],
+                    array(
+                        'attribute' => 'registration_ip',
+                        'value' => Html::a($model->registration_ip, 'http://ipinfo.io/'.$model->registration_ip, ['target' => '_blank']),
+                        'format' => 'raw',
+                        'visible' => User::hasPermission('viewRegistrationIp'),
+                    ),
+                    'created_at:datetime',
+                    'updated_at:datetime',
+                ],
+            ]) ?>
 
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
